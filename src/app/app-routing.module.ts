4ae28839f8ebc66a96e16cb52066b9451e2c0051
Данил Аникьев <anikyev95@mail.ru>
2019-09-20 10:09:40 +0400

    lab3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component'
+import { GoodsComponent } from './goods/goods.component'
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'goods', component: GoodsComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'admin', component: AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { GoodsComponent } from './goods/goods.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AdminComponent,
+    GoodsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
