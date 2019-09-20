aeec414fb84039811bbec98e7a54cac05a7c69a4
Данил Аникьев <anikyev95@mail.ru>
2019-09-20 08:41:35 +0400

    heroku server.js fix

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/front-end/dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
