36be6af6668a58988e402d0a3786b263aa48583c
Данил Аникьев <anikyev95@mail.ru>
2019-10-18 10:18:54 +0400

    lab4 admin page upgrade

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -12,6 +12,9 @@ export class AdminComponent implements OnInit {
 
   products: Product[] = [];
 
+  isUpdate: boolean = false;
+  product: Product = new Product();
+
   constructor(private httpClient: HttpClient) { }
 
   options = {
@@ -22,31 +25,26 @@ export class AdminComponent implements OnInit {
     this.httpClient.get(`${way}/goods`).subscribe((result: any) => this.products = result);
   }
 
-  buttonCreateClick(name: string, description: string, price: string, url: string) {
-    this.httpClient.post(`${way}/goods/create`, {
-      name: name,
-      description: description,
-      price: price,
-      url: url
-    }, this.options).subscribe((result: any) => {
+  buttonCreateClick() {
+    this.httpClient.post(`${way}/goods/create`, this.product, this.options).subscribe((result: any) => {
       if (!result) return;
       this.products.push({id: result.id, name: result.name, description: result.description, price: result.price, url: result.url});
     });
   }
 
-  buttonUpdateClick(id: number, name: string, description: string, price: string, url: string) {
-    this.httpClient.post(`${way}/goods/update`, {
-      id: id,
-      name: name,
-      description: description,
-      price: price,
-      url: url
-    }, this.options).subscribe((result: any) => {
+  buttonLoadUpdateClick(id: number) {
+    this.product = JSON.parse(JSON.stringify(this.products.find(x => x.id == id)));
+    this.isUpdate = true;
+  }
+
+  buttonUpdateClick() {
+    this.httpClient.post(`${way}/goods/update`, this.product, this.options).subscribe((result: any) => {
       if (!result) return;
       let productIndex = this.products.findIndex(x => x.id == result.id);
       if (productIndex == -1) return;
       this.products[productIndex] = result;
     });
+    this.isUpdate = false;
   }
 
   buttonDeleteClick(id: number) {
