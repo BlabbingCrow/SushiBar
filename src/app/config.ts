27412c0812c74f22998a2e68678076e635793fc5
Данил Аникьев <anikyev95@mail.ru>
2019-12-14 12:33:11 +0400

    lab 9 settings for heroku

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
 const way = 'https://sushibarback.herokuapp.com';
-//const way = 'http://localhost:3001';
-const wayWS = 'ws://sushibarback.herokuapp.com/';
-//const wayWS = 'ws://localhost:3002/';
+// const way = 'http://localhost:3001';
+const wayWS = 'wss://sushibarback.herokuapp.com';
+// const wayWS = 'ws://localhost:3002/';
 export {
     way,
     wayWS
