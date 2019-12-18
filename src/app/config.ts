c142a49a890a8a04b3ebf461b4306c593e23dad5
Данил Аникьев <anikyev95@mail.ru>
2019-12-18 09:14:38 +0400

    Some fixes

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
-const way = 'https://sushibarback.herokuapp.com/';
-// const way = 'http://localhost:3001/';
-const wayWS = 'wss://radiant-reef-23182.herokuapp.com/';
-// const wayWS = 'ws://localhost:3002/';
+const way = 'https://sushibarback.herokuapp.com';
+// const way = 'http://localhost:3001';
+const wayWS = 'wss://radiant-reef-23182.herokuapp.com';
+// const wayWS = 'ws://localhost:3002';
 export {
     way,
     wayWS
